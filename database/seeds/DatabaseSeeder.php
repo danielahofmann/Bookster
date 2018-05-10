cd7b869371e06942c264d21009a9ba390eed28ce
DHofmann <daniela.hofmann3010@gmail.com>
2018-05-10 21:30:10 +0200

    second test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(AuthorTableSeeder::class);
         $this->call(CategoryTableSeeder::class);
-        //$this->call(CustomerTableSeeder::class);
+        $this->call(CustomerTableSeeder::class);
         $this->call(GenreTableSeeder::class);
         $this->call(ProductTableSeeder::class);
     }
