27fbe9478bd105d51fac4cc9344e2df76bda60e8
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-02 16:17:39 +0200

    plugin: added vue-slider

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,6 +10,9 @@ require('./bootstrap');
 window.Vue = require('vue');
 Vue.config.devtools = true;
 
+import VueCarousel from 'vue-carousel';
+Vue.use(VueCarousel);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
