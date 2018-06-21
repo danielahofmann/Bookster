50a68f28040a8666c2c2fcd2b0151c9df28a3601
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-21 19:46:37 +0200

    added props and behavior if template is for older people. now loading specific classes if oldTemplate is true and hiding el when true

--- resources/assets/js/components/Logo.vue
+++ resources/assets/js/components/Logo.vue
@@ -1,54 +1,69 @@
 <template>
-    <div class="logo cell small-4 large-4">
-        <a href="/" class="logo-image"></a>
+    <div class="logo cell small-4 large-4" :class="{ 'medium-4' : isOld, 'hide-for-small-only' : isOld}">
+        <a href="/" class="logo-image" :class="{center : isOld}"></a>
     </div>
 </template>
 
 <script>
     export default {
         data() {
-            return {}
+            return {
+                isOld: this.oldTemplate,
+            }
         },
         mounted() {
             console.log('Logo ready');
         },
+        props: ['oldTemplate'],
     }
 </script>
 
 <style lang="scss" scoped>
     @import '~@/app.scss';
 
-    .logo{
-        .logo-image {
-            display: block;
-            background: url("/img/logo.png") no-repeat;
-            -webkit-background-size: 170px 30px;
-            background-size: 170px 30px;
-            height: 30px;
-            width: 170px;
-            margin-left: 50%;
-            transform: translateX(-50%);
+    .logo-image {
+        display: block;
+        background: url("/img/logo.png") no-repeat;
+        -webkit-background-size: 170px 30px;
+        background-size: 170px 30px;
+        height: 30px;
+        width: 170px;
+        margin-left: 50%;
+        transform: translateX(-50%);
 
-            @include custom-max(1024px){
-                margin-left: 0;
-                transform: translateX(0);
-            }
-
-            @include custom-max(430px){
-                -webkit-background-size: 120px 20px;
-                background-size: 120px 20px;
-                height: 20px;
-                width: 120px;
-                margin-top: 10px;
-            }
-
-            @include custom-max(340px){
-                -webkit-background-size: 100px 17px;
-                background-size: 100px 17px;
-                height: 17px;
-                width: 100px;
-            }
+        @include custom-max(1024px) {
+            margin-left: 0;
+            transform: translateX(0);
         }
 
+        @include custom-max(430px) {
+            -webkit-background-size: 120px 20px;
+            background-size: 120px 20px;
+            height: 20px;
+            width: 120px;
+            margin-top: 10px;
+        }
+
+        @include custom-max(340px) {
+            -webkit-background-size: 100px 17px;
+            background-size: 100px 17px;
+            height: 17px;
+            width: 100px;
+        }
+    }
+
+    .center {
+        display: block;
+        background: url("/img/logo.png") no-repeat;
+        -webkit-background-size: 170px 30px;
+        background-size: 170px 30px;
+        height: 30px;
+        width: 170px;
+        margin-left: 50%;
+        transform: translateX(-50%);
+
+        @include custom-max(1024px) {
+            @include margin-left;
+        }
     }
 </style>
