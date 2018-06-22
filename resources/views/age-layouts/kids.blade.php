dbdc8046eaac8473bf58cd68737bed77c8a0f6ab
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-22 10:00:55 +0200

    defined general layout for kids

--- resources/views/age-layouts/kids.blade.php
+++ resources/views/age-layouts/kids.blade.php
@@ -5,15 +5,40 @@
 @stop
 
 @section('body')
-    <header>
+    <offcanvas
+            size="1.25"
+    ></offcanvas>
 
-    </header>
+    <div id="offcanvas-background" class="offcanvas-background closed"></div>
+    <offcanvas-close></offcanvas-close>
 
-    <main>
+    <div class="off-canvas-content" data-off-canvas-content>
+        <header class="grid-x padding-top-small header clearfix">
+            <mobile-logo></mobile-logo>
+            <hamburger-menu
+                    :old-template='true'
+            ></hamburger-menu>
+            <search @search="search" class="display-none-tablet"></search>
+            <logo
+                    :old-template='true'
+            ></logo>
+            <div class="cell small-6 medium-4 large-4">
+                <div class="grid-x align-right">
+                    <wishlist></wishlist>
+                </div>
+            </div>
 
-    </main>
+            <navigation
+                    :is-active='true'
+                    size="1.25"
+                    class="display-none-tablet"
+            ></navigation>
+        </header>
 
-    <footer>
+        <main class="main">
+            @yield('main')
+        </main>
 
-    </footer>
+        <footer-section></footer-section>
+    </div>
 @stop
--- resources/views/age-layouts/kids/home.blade.php
+++ resources/views/age-layouts/kids/home.blade.php
@@ -2,6 +2,13 @@
 
 @section('title', 'Startseite' )
 
-@section('nav')
-    <p>heeey</p>
+@section('main')
+    <slider></slider>
+
+    <h2>Aktuelle Bestseller</h2>
+    <book-carousel></book-carousel>
+
+    <section class="grid-x flex-center">
+        <h2 class="cell small-12 text-center" style="padding:0 0 40px 0">Neuheiten</h2>
+    </section>
 @stop
