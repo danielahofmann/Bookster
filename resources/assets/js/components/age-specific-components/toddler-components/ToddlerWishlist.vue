1a9ac270335e8a4e622aaef86e727255925ec783
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-22 16:04:29 +0200

    added template and styling to wishlist component for toddlers

--- resources/assets/js/components/age-specific-components/toddler-components/ToddlerWishlist.vue
+++ resources/assets/js/components/age-specific-components/toddler-components/ToddlerWishlist.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="">
-
+    <div class="toddler-wishlist">
+        <a href="/wishlist/" class="toddler-wishlist-icon"></a>
     </div>
 </template>
 
@@ -9,14 +9,32 @@
         data() {
             return {}
         },
-        mounted() {
-            console.log('Component ready');
-        },
+        mounted() {},
     }
 </script>
 
 <style lang="scss" scoped>
     @import '~@/app.scss';
 
+    .toddler-wishlist {
+        display: inline-block;
+        .toddler-wishlist-icon {
+            display: block;
+            background: url(/img/toddler-wishlist.svg) no-repeat;
+            background-size: 40px 40px;
+            height: 40px;
+            width: 40px;
+            padding-right: 5rem;
+            margin-bottom: 15px;
 
+            &:hover{
+                background: url(/img/wishlist-add-kids.svg) no-repeat;
+                background-size: 40px 40px;
+            }
+
+            @include custom-max(640px){
+                margin-bottom: 0;
+            }
+        }
+    }
 </style>
