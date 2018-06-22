6e6f73e8d1a7f8fc4c555232f0882cc1ebbd5e62
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-22 16:03:22 +0200

    added styling and template to opt-out component

--- resources/assets/js/components/age-specific-components/toddler-components/ToddlerFooterOptOut.vue
+++ resources/assets/js/components/age-specific-components/toddler-components/ToddlerFooterOptOut.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="">
-
+    <div class="small-12 text-center">
+        <a href="/default/" class="opt-out">Zurück zur Standardansicht</a>
     </div>
 </template>
 
@@ -18,5 +18,20 @@
 <style lang="scss" scoped>
     @import '~@/app.scss';
 
+    div{
+        background: $beige;
+        padding: 20px 0;
+
+        a{
+            color: $light-grey;
+            @include text-styling($primary-font, $light, 12px);
+            text-align: center;
+
+            &:hover{
+                color: $dark-grey;
+                font-weight: $regular;
+            }
+        }
+    }
 
 </style>
