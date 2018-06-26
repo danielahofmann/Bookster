bdc94cbcae51bf70220f5a0108a9a9a7776a241f
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-26 16:35:13 +0200

    defined relationships between author and category

--- app/Author.php
+++ app/Author.php
@@ -20,4 +20,8 @@ class Author extends Model
 	public function author_image(  ) {
 		return $this->hasOne('App\AuthorImage');
 	}
+
+	public function categories() {
+		return $this->belongsToMany('App\Category', 'authors_categories')->withTimestamps()->withPivot('category_id', 'author_id');
+	}
 }
--- app/Category.php
+++ app/Category.php
@@ -21,4 +21,8 @@ class Category extends Model
 	{
 		return $this->hasMany('App\Genre');
 	}
+
+	public function authors() {
+		return $this->belongsToMany('App\Author', 'authors_categories')->withTimestamps()->withPivot('category_id', 'author_id');
+	}
 }
