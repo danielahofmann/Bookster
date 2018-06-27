6b63468ffc5436f2b62c06d152b9a01be76d621c
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-27 13:12:35 +0200

    added same logic for handling cart and saving this data to session also

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cart;
+use App\Product;
+use Session;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+	/**
+	 * @param $id
+	 *
+	 * @param  Request  $request
+	 * @return array
+	 */
+	public function saveProductToCart(Request $request, $id) {
+		$product = Product::find($id);
+		$oldCart = Session::has('cart') ? Session::get('cart') : null;
+
+		$cart = new Cart($oldCart);
+		$cart->add($product, $product->id);
+
+		$request->session()->put('cart', $cart);
+
+		$session_data = [
+			'cart' => session('cart'),
+		];
+
+		return $session_data;
+	}
+}
