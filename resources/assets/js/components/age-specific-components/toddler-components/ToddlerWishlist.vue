bdc2070c493d0351e31c5e1233ece4c62930ba71
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-28 17:30:20 +0200

    added the wishlist logic to toddlerswishlist

--- resources/assets/js/components/age-specific-components/toddler-components/ToddlerWishlist.vue
+++ resources/assets/js/components/age-specific-components/toddler-components/ToddlerWishlist.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="toddler-wishlist">
-        <a href="/wishlist/" class="toddler-wishlist-icon"></a>
+        <a href="/wishlist/" class="toddler-wishlist-icon" :class="{ true : wishlist, false : wishlistInverse }"></a>
     </div>
 </template>
 
@@ -9,7 +9,36 @@
         data() {
             return {}
         },
-        mounted() {},
+        mounted() {
+            var self = this;
+
+            axios
+                .get('/api/getWishlistQuantity/')
+                .then(function (response) {
+                    const quantity = response.data;
+
+                    if(quantity > 0){
+                        self.$store.commit('newWishlistItem', quantity);
+                    }
+
+                    self.$store.commit('setQuantity', quantity);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        computed: {
+            wishlist(){
+                return this.$store.state.wishlist;
+            },
+            wishlistInverse(){
+                if (this.$store.state.wishlist === true){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        }
     }
 </script>
 
@@ -20,8 +49,6 @@
         display: inline-block;
         .toddler-wishlist-icon {
             display: block;
-            background: url(/img/toddler-wishlist.svg) no-repeat;
-            background-size: 40px 40px;
             height: 40px;
             width: 40px;
             padding-right: 5rem;
@@ -35,6 +62,16 @@
             @include custom-max(640px){
                 margin-bottom: 0;
             }
+        }
+
+        .false {
+            background: url(/img/toddler-wishlist.svg) no-repeat;
+            background-size: 40px 40px;
+        }
+
+        .true {
+            background: url(/img/wishlist-true-kids.svg) no-repeat;
+            background-size: 40px 40px;
         }
     }
 </style>
