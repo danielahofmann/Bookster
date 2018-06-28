76763e660939616191bf8b4f4e0a4edc25ee8a50
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-28 17:32:34 +0200

    added component which shows the characters image, so kids know who's products they see

---
+++ resources/assets/js/components/age-specific-components/toddler-components/ToddlerCharacter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="grid-x flex-center character-image">
+        <img :src="character.character_image.img" :alt="character.name" class="small-4 medium-3 large-2">
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                character: this.thisCharacter,
+            }
+        },
+        mounted() {},
+        props:['thisCharacter']
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/app.scss';
+
+    .character-image{
+        img{
+            border-radius: 50%;
+            margin-bottom: 50px;
+
+            @include custom-max(1023px){
+                margin-top: 50px;
+            }
+        }
+    }
+
+</style>
