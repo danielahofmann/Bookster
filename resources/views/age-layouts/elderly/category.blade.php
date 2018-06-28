62375e2d83803c171e793a1568f662e9b01c29c2
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-28 13:32:51 +0200

    added category for elderly

---
+++ resources/views/age-layouts/elderly/category.blade.php
@@ -0,0 +1,25 @@
+@extends ('age-layouts.elderly')
+
+@section('title', 'Kategorie' )
+
+@section('main')
+    <div>
+        <h2>{{$category->name}}</h2>
+
+        <filter-category
+                :category-genres="{{$genres}}"
+                :category-authors="{{$authors}}"
+                :category-id="{{$category->id}}"
+                :size="1.25"
+                @filter="filterGenre"
+                @nofilter="noFilter"
+        ></filter-category>
+
+        <book-preview-section
+                :category-id="{{$category->id}}"
+                :fontsize="1"
+                :parent-products="products"
+                @update="updateProducts"
+        ></book-preview-section>
+    </div>
+@stop
