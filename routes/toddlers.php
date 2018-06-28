71db7064fe6ad79a76c217bb6be4275b85aa20bc
DHofmann <daniela.hofmann3010@gmail.com>
2018-06-28 17:31:59 +0200

    added the route logic for toddlers character, getting the character data and the products of the character

--- routes/toddlers.php
+++ routes/toddlers.php
@@ -5,16 +5,10 @@ Route::prefix('toddlers')->group(function() {
 		return view('age-layouts.toddlers.home');
 	} );
 
-	Route::get( '/category/{category_id}', function ($category_id) {
-		$category = App\Category::find($category_id);
-		$genres = App\Genre::where('category_id', $category_id)->get();
+	Route::get( '/character/{character_id}', function ($character_id) {
+		$character = App\Character::where('id', $character_id)->with('character_image')->get();
+		$products = App\Product::where('character_id', $character_id)->with('image')->get();
 
-		$filterCategory = function($query) use ($category_id) {
-			$query->where('category_id', $category_id);
-		};
-
-		$authors = App\Author::whereHas('categories', $filterCategory)->with(['categories' => $filterCategory])->get();
-
-		return view('age-layouts.toddlers.category', ['category' => $category, 'genres' => $genres, 'authors' => $authors]);
-	});
+		return view('age-layouts.toddlers.category', ['character' => $character[0], 'products' => $products]);
+	})->name('character');
 });
