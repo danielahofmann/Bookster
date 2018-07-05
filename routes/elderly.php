f620f04c32a6cb03e6dcd546c4b46d214c884a9f
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-05 18:46:04 +0200

    responsive wishlist elderly

--- routes/elderly.php
+++ routes/elderly.php
@@ -30,4 +30,15 @@ Route::prefix('elderly')->group(function() {
 
 		return view('age-layouts.elderly.product', ['product' => $product]);
 	})->name('elderly-product');
+
+	Route::get('/wishlist', function() {
+		if(!Session::has('wishlist')){
+			return view('age-layouts.elderly.wishlist', ['products' => null]);
+		}
+
+		$oldWishlist = Session::get('wishlist');
+		$wishlist = new App\WishlistSession($oldWishlist);
+
+		return view('age-layouts.elderly.wishlist', ['products' => $wishlist->items]);
+	})->name('kids-wishlist');
 });
