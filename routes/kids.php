714ab06f5e4979c23dbfb1bdf2203917b632db2e
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-05 18:46:22 +0200

    responsive wishlist kids (for today, layout needs some changes)

--- routes/kids.php
+++ routes/kids.php
@@ -27,4 +27,15 @@ Route::prefix('kids')->group(function() {
 
 		return view('age-layouts.kids.product', ['product' => $product]);
 	})->name('kids-product');
+
+	Route::get('/wishlist', function() {
+		if(!Session::has('wishlist')){
+			return view('age-layouts.kids.wishlist', ['products' => null]);
+		}
+
+		$oldWishlist = Session::get('wishlist');
+		$wishlist = new App\WishlistSession($oldWishlist);
+
+		return view('age-layouts.kids.wishlist', ['products' => $wishlist->items]);
+	})->name('kids-wishlist');
 });
