bb21108db97dda64694b050e62057746b70ddec4
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-05 18:45:54 +0200

    responsive wishlist teens

--- routes/teens.php
+++ routes/teens.php
@@ -29,4 +29,15 @@ Route::prefix('teens')->group(function() {
 
 		return view('age-layouts.teens.product', ['product' => $product]);
 	})->name('teens-product');
+
+	Route::get('/wishlist', function() {
+		if(!Session::has('wishlist')){
+			return view('age-layouts.teens.wishlist', ['products' => null]);
+		}
+
+		$oldWishlist = Session::get('wishlist');
+		$wishlist = new App\WishlistSession($oldWishlist);
+
+		return view('age-layouts.teens.wishlist', ['products' => $wishlist->items]);
+	})->name('teens-wishlist');
 });
