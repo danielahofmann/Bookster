57be8b3763f5924d9dc49d4eae3c4469ed5c1cc9
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-06 15:36:57 +0200

    set routes for sending mail and getting wishlist

--- routes/toddlers.php
+++ routes/toddlers.php
@@ -21,4 +21,19 @@ Route::prefix('toddlers')->group(function() {
 
 		return view('age-layouts.toddlers.product', ['product' => $product]);
 	})->name('toddlers-product');
+
+	Route::get('/wishlist', function() {
+		if(!Session::has('wishlist')){
+			return view('age-layouts.toddlers.wishlist', ['products' => null]);
+		}
+
+		$oldWishlist = Session::get('wishlist');
+		$wishlist = new App\WishlistSession($oldWishlist);
+
+		return view('age-layouts.toddlers.wishlist', ['products' => $wishlist->items]);
+	})->name('toddlers-wishlist');
+
+	Route::get('/send', function(){
+		return view('age-layouts.toddlers.send');
+	})->name('toddlers-send-wishlist');
 });
