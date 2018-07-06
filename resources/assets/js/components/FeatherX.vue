9f3df4947dc7c3cf218e844ce3af6830aeaee2ef
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-06 20:34:33 +0200

    added some feather icons

---
+++ resources/assets/js/components/FeatherX.vue
@@ -0,0 +1,30 @@
+<template>
+    <x-icon></x-icon>
+</template>
+
+<script>
+    import { X } from 'vue-feather-icon'
+
+    export default {
+        data() {
+            return {}
+        },
+        mounted() {},
+        components: {
+            XIcon: X
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/app.scss';
+
+    svg{
+        width: 20px;
+        stroke: $light-grey;
+
+        &:hover{
+            stroke: $dark-grey;
+        }
+    }
+</style>
