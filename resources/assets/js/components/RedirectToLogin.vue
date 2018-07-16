7e1128f2edc5d6d73a1dc5460a124a8ff4841296
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-16 15:58:57 +0200

    build components, which save key to session, so later on you can check if key exists and then redirect to another view (which will be checkout, when logged in or registered from checkout beforehand)

---
+++ resources/assets/js/components/RedirectToLogin.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="cell small-12 medium-3 large-2  flex-center" @click="saveCheckoutToSession()">
+        <feather-user></feather-user>
+        <p>Jetzt einloggen</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        mounted() {
+            console.log('Component ready');
+        },
+        methods: {
+            saveCheckoutToSession: function(){
+                axios
+                    .get('/api/saveCheckoutToSession')
+                    .then(response => (
+                       window.location = '/' +response.data + '/login'))
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/app.scss';
+
+    div{
+        background-color: $white;
+        padding: 1.5rem;
+        margin-right: 2rem;
+        margin-bottom: 10rem;
+
+        @include custom-max(639px){
+            margin: 0;
+        }
+
+        p{
+            @include text-styling($primary-font, $regular, 1rem);
+            color: $dark-grey;
+            text-align: center;
+            margin: 0;
+            display: inline-block;
+            padding-left: 1.5rem;
+        }
+
+        svg{
+            display: inline-block;
+        }
+    }
+</style>
---
+++ resources/assets/js/components/RedirectToRegistration.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="cell small-12 medium-3 large-2 flex-center" @click="saveCheckoutToSession()">
+        <p class="add">+</p>
+        <p>Jetzt registrieren</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        mounted() {},
+        methods: {
+            saveCheckoutToSession: function(){
+                axios
+                    .get('/api/saveCheckoutToSession')
+                    .then(response => (
+                        window.location = '/' +response.data + '/register'))
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/app.scss';
+
+    div{
+        background-color: $white;
+        padding: 1.5rem;
+        margin-bottom: 10rem;
+
+        @include custom-max(639px){
+            margin-bottom: 3rem;
+        }
+
+        p{
+            @include text-styling($primary-font, $regular, 1rem);
+            color: $dark-grey;
+            text-align: center;
+            margin: 0;
+            display: inline-block;
+        }
+
+        .add{
+            display: inline-block;
+            padding-right: 1.5rem;
+            font-size: 1.5rem;
+            font-weight: $bold;
+        }
+    }
+</style>
