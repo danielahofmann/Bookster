395a4513ce27a76c94253fd945e2ee2e4f6e51e4
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-16 20:21:35 +0200

    added content to component, now when choosing a delivery option, the price gets added to subtotal and saved to totalPrice in store.js

--- resources/assets/js/components/OrderDelivery.vue
+++ resources/assets/js/components/OrderDelivery.vue
@@ -1,6 +1,29 @@
 <template>
-    <div class="">
+    <div class="cell small-12 medium-8 grid-x order-box">
+        <div class="cell small-12">
+            <h4>Versandoptionen</h4>
+            <div class="delivery-option grid-x">
+                <p class="cell small-2 delivery-price">5,00 €</p>
+                <div class="cell small-8 delivery-information">
+                    <p class="title">Standardversand</p>
+                    <p class="information">Lieferung innerhalb von 3-4 Werktagen</p>
+                </div>
+                <div class="cell small-2 delivery-radio">
+                    <input type="radio" name="delivery" id="delivery" @change="setDelivery(5)">
+                </div>
+            </div>
 
+            <div class="delivery-option grid-x">
+                <p class="cell small-2 delivery-price">8,00 €</p>
+                <div class="cell small-8 delivery-information">
+                    <p class="title">Expressversand</p>
+                    <p class="information">Lieferung innerhalb von 1-2 Werktagen</p>
+                </div>
+                <div class="cell small-2 delivery-radio">
+                    <input type="radio" name="delivery" id="delivery" @change="setDelivery(8)">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,9 +32,15 @@
         data() {
             return {}
         },
-        mounted() {
-            console.log('Component ready');
-        },
+        mounted() {},
+        methods: {
+            setDelivery: function (deliveryPrice){
+                this.$store.commit('setDeliveryPrice', deliveryPrice);
+                this.$store.commit('addDeliveryPriceToTotalPrice', deliveryPrice);
+
+
+            }
+        }
     }
 </script>
 
