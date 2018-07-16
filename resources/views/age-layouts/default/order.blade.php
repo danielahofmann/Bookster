93019ca3f72608049f3ba3385c4355916bc319ce
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-16 20:20:23 +0200

    added components to blade view

--- resources/views/age-layouts/default/order.blade.php
+++ resources/views/age-layouts/default/order.blade.php
@@ -7,143 +7,117 @@
     <section class="fullscreen-beige-background">
         <div class="grid-container">
             <div class="grid-x grid-margin-x">
+
                 <h2 class="cell small-12">ZUR KASSE</h2>
 
-                <div class="cell small-12 medium-8 grid-x order-box">
-                    <div class="cell small-10">
-                        <h4>Versandadresse</h4>
-                        <p>{{$customer->firstname}} {{$customer->lastname}}</p>
-                        <p>{{$customer->street}} {{$customer->housenum}}</p>
-                        <p>{{$customer->postcode}} {{$customer->city}}</p>
-                        <p>{{$customer->email}}</p>
+                <div class="cell small-12 medium-8">
+                    <div class="cell small-12 medium-8 grid-x order-box">
+                        <div class="cell small-10">
+                            <h4>Versandadresse</h4>
+                            <p>{{$customer->firstname}} {{$customer->lastname}}</p>
+                            <p>{{$customer->street}} {{$customer->housenum}}</p>
+                            <p>{{$customer->postcode}} {{$customer->city}}</p>
+                            <p>{{$customer->email}}</p>
+                        </div>
+
+                        <div class="cell small-2">
+                            <button class="order-button">bearbeiten</button>
+                        </div>
                     </div>
 
-                    <div class="cell small-2">
-                        <button class="order-button">bearbeiten</button>
+                    <order-delivery></order-delivery>
+
+                    <div class="cell small-12 medium-8 grid-x order-box">
+                        <div class="cell small-10">
+                            <h4>Zahlung</h4>
+
+                            <h5>Rechnungsanschrift</h5>
+                            <p>{{$customer->firstname}} {{$customer->lastname}}</p>
+                            <p>{{$customer->street}} {{$customer->housenum}}</p>
+                            <p>{{$customer->postcode}} {{$customer->city}}</p>
+                            <p>{{$customer->email}}</p>
+                        </div>
+
+                        <div class="cell small-2">
+                            <button class="order-button">bearbeiten</button>
+                        </div>
+
+
+                        <h5 class="cell small-12">Zahlungsart</h5>
+
+                        <div class="cell payment small-12 grid-x">
+                            <div class="cell small-10 payment-information">
+                                <p class="title">Rechnung</p>
+                                <p class="information">Rechnungsbetrag innerhalb von 2 Wochen überweisen</p>
+                            </div>
+                            <div class="cell small-2">
+                                <input type="radio" name="payment" id="payment">
+                            </div>
+                        </div>
+
+                        <div class="cell payment small-12 grid-x">
+                            <div class="cell small-10 payment-information">
+                                <p class="title">Vorkasse</p>
+                                <p class="information">Ihre Bestellung wird versandt, wenn Sie den Betrag überwiesen
+                                    haben</p>
+                            </div>
+                            <div class="cell small-2">
+                                <input type="radio" name="payment" id="payment">
+                            </div>
+                        </div>
+
+                        <div class="cell payment small-12 grid-x">
+                            <div class="cell small-10 payment-information">
+                                <p class="title">Nachname</p>
+                                <p class="information">Rechnung wird bei Lieferung bezahlt</p>
+                            </div>
+                            <div class="cell small-2">
+                                <input type="radio" name="payment" id="payment">
+                            </div>
+                        </div>
                     </div>
+
+                    <div class="cell small-12 medium-8 grid-x order-box">
+                        <div class="cell small-10">
+                            <h4>Allgemeine Geschäftsbedingungen</h4>
+                            <p class="information">Mit der Übermittlung der für die Abwicklung der gewählten Klarna
+                                Zahlungsmethode und einer Identitäts- und Bonitätsprüfung erforderlichen Daten an Klarna
+                                bin ich einverstanden. Meine Einwilligung kann ich jederzeit mit Wirkung für die Zukunft
+                                widerrufen. Es gelten die AGB des Händlers Rechnungsbedingungen. </p>
+                        </div>
+
+                        <div class="cell small-2 flex-center">
+                            <input type="checkbox" name="agb" id="agb">
+                        </div>
+                    </div>
+
+                    <button type="submit" class="submit-button">Jetzt Bestellen</button>
                 </div>
-
 
                 <div class="cell small-12 medium-4 order-box">
+                    <div class="">
+                        <h4>Artikel</h4>
 
-                    <h4>Artikel</h4>
+                        @foreach($products as $product)
+                            <product-order
+                                    key="{{$product['item']['id']}}"
+                                    title="{{$product['item']['name']}}"
+                                    price="{{$product['item']['price']}}"
+                                    img="{{$product['item']['image'][0]['img']}}"
+                                    author="{{$product['item']['author']['firstname'] . ' ' . $product['item']['author']['lastname']}}"
+                                    quantity="{{$product['quantity']}}"
+                                    id="{{$product['item']['id']}}"
+                                    size="1"
+                            ></product-order>
+                        @endforeach
 
-                    @foreach($products as $product)
-                        <product-order
-                                key="{{$product['item']['id']}}"
-                                title="{{$product['item']['name']}}"
-                                price="{{$product['item']['price']}}"
-                                img="{{$product['item']['image'][0]['img']}}"
-                                author="{{$product['item']['author']['firstname'] . ' ' . $product['item']['author']['lastname']}}"
-                                quantity="{{$product['quantity']}}"
-                                id="{{$product['item']['id']}}"
-                                size="1"
-                        ></product-order>
-                    @endforeach
+                        <hr>
 
-                    <hr>
-
-                    <div class="cell small-12 grid-x">
-                        <p class="cell small-8">Zwischensumme:</p>
-                        <p class="cell small-4">{{ $totalPrice }} €</p>
-                        <p class="cell small-8">Versand:</p>
-                        <p class="cell small-4">5,00 €</p>
-
-                        <p class="cell small-8">GESAMT:</p>
-                        <p class="cell small-4">{{ $totalPrice }} €</p>
-
+                        <order-bill
+                                :sub-total="{{$totalPrice}}"
+                        ></order-bill>
                     </div>
                 </div>
-
-                <div class="cell small-12 medium-8 grid-x order-box">
-                    <div class="cell small-12">
-                        <h4>Versandoptionen</h4>
-                        <div class="delivery-option grid-x">
-                            <p class="cell small-2 delivery-price">5,00 €</p>
-                            <div class="cell small-8 delivery-information">
-                                <p class="title">Standardversand</p>
-                                <p class="information">Lieferung innerhalb von 3-4 Werktagen</p>
-                            </div>
-                            <div class="cell small-2 delivery-radio">
-                                <input type="radio" name="delivery" id="delivery">
-                            </div>
-                        </div>
-
-                        <div class="delivery-option grid-x">
-                            <p class="cell small-2 delivery-price">8,00 €</p>
-                            <div class="cell small-8 delivery-information">
-                                <p class="title">Expressversand</p>
-                                <p class="information">Lieferung innerhalb von 1-2 Werktagen</p>
-                            </div>
-                            <div class="cell small-2 delivery-radio">
-                                <input type="radio" name="delivery" id="delivery">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="cell small-12 medium-8 grid-x order-box">
-                    <div class="cell small-10">
-                        <h4>Zahlung</h4>
-
-                        <h5>Rechnungsanschrift</h5>
-                        <p>{{$customer->firstname}} {{$customer->lastname}}</p>
-                        <p>{{$customer->street}} {{$customer->housenum}}</p>
-                        <p>{{$customer->postcode}} {{$customer->city}}</p>
-                        <p>{{$customer->email}}</p>
-                    </div>
-
-                    <div class="cell small-2">
-                        <button class="order-button">bearbeiten</button>
-                    </div>
-
-
-
-                    <h5 class="cell small-12">Zahlungsart</h5>
-
-                    <div class="cell payment small-12 grid-x">
-                        <div class="cell small-10 payment-information">
-                            <p class="title">Rechnung</p>
-                            <p class="information">Rechnungsbetrag innerhalb von 2 Wochen überweisen</p>
-                        </div>
-                        <div class="cell small-2">
-                            <input type="radio" name="payment" id="payment">
-                        </div>
-                    </div>
-
-                    <div class="cell payment small-12 grid-x">
-                        <div class="cell small-10 payment-information">
-                            <p class="title">Vorkasse</p>
-                            <p class="information">Ihre Bestellung wird versandt, wenn Sie den Betrag überwiesen haben</p>
-                        </div>
-                        <div class="cell small-2">
-                            <input type="radio" name="payment" id="payment">
-                        </div>
-                    </div>
-
-                    <div class="cell payment small-12 grid-x">
-                        <div class="cell small-10 payment-information">
-                            <p class="title">Nachname</p>
-                            <p class="information">Rechnung wird bei Lieferung bezahlt</p>
-                        </div>
-                        <div class="cell small-2">
-                            <input type="radio" name="payment" id="payment">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="cell small-12 medium-8 grid-x order-box">
-                    <div class="cell small-10">
-                        <h4>Allgemeine Geschäftsbedingungen</h4>
-                        <p class="information">Mit der Übermittlung der für die Abwicklung der gewählten Klarna Zahlungsmethode und einer Identitäts- und Bonitätsprüfung erforderlichen Daten an Klarna bin ich einverstanden. Meine Einwilligung kann ich jederzeit mit Wirkung für die Zukunft widerrufen. Es gelten die AGB des Händlers Rechnungsbedingungen.  </p>
-                    </div>
-
-                    <div class="cell small-2 flex-center">
-                        <input type="checkbox" name="agb" id="agb">
-                    </div>
-                </div>
-
-                <button type="submit" class="submit-button">Jetzt Bestellen</button>
             </div>
         </div>
     </section>
