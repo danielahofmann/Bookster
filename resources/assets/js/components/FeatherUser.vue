0aa794bc3a644c0c270fd7232d4b0b663143bf2c
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-16 15:59:06 +0200

    added feather icons

---
+++ resources/assets/js/components/FeatherUser.vue
@@ -0,0 +1,28 @@
+<template>
+    <user-icon></user-icon>
+</template>
+
+<script>
+    import { User } from 'vue-feather-icon'
+
+    export default {
+        data() {
+            return {}
+        },
+        mounted() {
+        },
+        components: {
+            UserIcon: User
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/app.scss';
+
+    svg{
+        width: 25px;
+        height: 25px;
+        stroke: $blue !important;
+    }
+</style>
