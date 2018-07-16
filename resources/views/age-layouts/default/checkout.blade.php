3d9588721807d8cab8b3266a58c3c042302bf0f4
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-16 16:47:31 +0200

    added text for better understanding

--- resources/views/age-layouts/default/checkout.blade.php
+++ resources/views/age-layouts/default/checkout.blade.php
@@ -5,6 +5,7 @@
 @section('main')
     <section class="fullscreen-beige-background grid-x flex-center">
         <h2 class="cell small-12">Jetzt bestellen</h2>
+        <p class="cell small-12 text-checkout">Loggen Sie sich jetzt in Ihr bestehendes Konto ein, oder registrieren Sie sich als Neukunde um die Bestellung durchzuführen.</p>
 
         <redirect-login></redirect-login>
         <redirect-registration></redirect-registration>
