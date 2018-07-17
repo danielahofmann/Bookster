24b5b549f7da2286c793e657ab4e2c939c6b75b6
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-17 19:26:10 +0200

    added popup component, which dynamically fades out after 3000ms

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -94,6 +94,7 @@ Vue.component('feather-user', require('./components/FeatherUser.vue'));
 Vue.component('feather-user-plus', require('./components/FeatherUserPlus.vue'));
 Vue.component('redirect-login', require('./components/RedirectToLogin.vue'));
 Vue.component('redirect-registration', require('./components/RedirectToRegistration.vue'));
+Vue.component('alert-success-popup', require('./components/AlertSuccessPopup.vue'));
 
 
 
