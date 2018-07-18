51aa80f86df3e19eb7a2132512fdf28314a91923
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-18 16:49:24 +0200

    edited the order-success default, so its now pretty and responsive

--- resources/views/age-layouts/default/order-success.blade.php
+++ resources/views/age-layouts/default/order-success.blade.php
@@ -3,8 +3,12 @@
 @section('title', 'Bestellung erfolgreich' )
 
 @section('main')
-    @if(session('status'))
-        <p>{{session('status')}}</p>
-    @endif
+    <section class="fullscreen-beige-background grid-x flex-center">
 
+        <h2 class="cell small-12">Bestellung erfolgreich</h2>
+        <div class="cell small-12 grid-x flex-center">
+            <p class="cell small-12 medium-6 text-center">Ihre Bestellung ist nun bei uns eingegangen und wird schnellstens von uns bearbeitet. Vielen Dank für Ihren Einkauf bei bookster.</p>
+        </div>
+        <a href="{{route('default-home')}}" class="button">Zur Startseite</a>
+    </section>
 @endsection
