0ad6a5f672f9fdf8d5952a3cb5301f5c60f82cc4
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-18 14:34:02 +0200

    added new db for approvals for teens

---
+++ database/migrations/2018_07_18_122406_create_approvals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApprovalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('approvals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id')->length(11);
+            $table->integer('order_id')->length(11);
+            $table->string('email', 128);
+            $table->boolean('confirmed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('approvals');
+    }
+}
