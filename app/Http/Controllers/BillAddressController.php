7fb0145b83d41fb9a8084c93a5ff832ede7b9786
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-18 11:15:49 +0200

    added store logic for Delivery/BillAddress which either will save the data from the session, or if those are empty use data from Auth::user

---
+++ app/Http/Controllers/BillAddressController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BillAddress;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class BillAddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+	    if(Session::has('billAddress')){
+		    $bill = Session::get('billAddress');
+
+		    $billAddress = new BillAddress();
+		    $billAddress->firstname = $bill['firstname'];
+		    $billAddress->lastname = $bill['lastname'];
+		    $billAddress->street = $bill['street'];
+		    $billAddress->housenum = $bill['housenum'];
+		    $billAddress->city = $bill['city'];
+		    $billAddress->postcode = $bill['postcode'];
+		    $billAddress->email = $bill['email'];
+		    $billAddress->save();
+	    }else{
+		    $billAddress = new BillAddress();
+		    $billAddress->firstname = Auth::user()->firstname;
+		    $billAddress->lastname =  Auth::user()->lastname;
+		    $billAddress->street =  Auth::user()->street;
+		    $billAddress->housenum =  Auth::user()->housenum;
+		    $billAddress->city =  Auth::user()->city;
+		    $billAddress->postcode =  Auth::user()->postcode;
+		    $billAddress->email =  Auth::user()->email;
+		    $billAddress->save();
+	    }
+
+	    return $billAddress->id;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/DeliveryAddressController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DeliveryAddress;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class DeliveryAddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+	public function store()
+	{
+		if(Session::has('billAddress')){
+			$delivery = Session::get('deliveryAddress');
+
+			$deliveryAddress = new DeliveryAddress();
+			$deliveryAddress->firstname = $delivery['firstname'];
+			$deliveryAddress->lastname = $delivery['lastname'];
+			$deliveryAddress->street = $delivery['street'];
+			$deliveryAddress->housenum = $delivery['housenum'];
+			$deliveryAddress->city = $delivery['city'];
+			$deliveryAddress->postcode = $delivery['postcode'];
+			$deliveryAddress->email = $delivery['email'];
+			$deliveryAddress->save();
+		}else{
+			$deliveryAddress = new DeliveryAddress();
+			$deliveryAddress->firstname = Auth::user()->firstname;
+			$deliveryAddress->lastname =  Auth::user()->lastname;
+			$deliveryAddress->street =  Auth::user()->street;
+			$deliveryAddress->housenum =  Auth::user()->housenum;
+			$deliveryAddress->city =  Auth::user()->city;
+			$deliveryAddress->postcode =  Auth::user()->postcode;
+			$deliveryAddress->email =  Auth::user()->email;
+			$deliveryAddress->save();
+		}
+
+		return $deliveryAddress->id;
+	}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
