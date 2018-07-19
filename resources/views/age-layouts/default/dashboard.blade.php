d1d8fc6a8376e33a1fb6324f0698685fa373b4c9
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-19 16:08:02 +0200

    added component to default template

--- resources/views/age-layouts/default/dashboard.blade.php
+++ resources/views/age-layouts/default/dashboard.blade.php
@@ -10,54 +10,15 @@
                     <img src="/img/dashboard-welcome.png" alt="Willkommen im Dashboard" class="dashboard-image">
                 </div>
 
-                <div class="cell small-12 medium-6 large-4">
-                    <div class="welcome-user">
-                        <p class="dashboard-initial">
-                            @php($firstchar =  substr($customer->firstname, 0, 1))
-                            @php($scndchar =  substr($customer->lastname, 0, 1))
-                            {{$firstchar}}{{$scndchar}}
-                        </p>
-                        <div class="welcome-text">
-                            <p class="dashboard-regular">Hallo,</p>
-                            <p class="dashboard-bold">{{$customer->firstname}} {{$customer->lastname}}</p>
-                        </div>
-                    </div>
-                    <div class="dashboard-menu border-left display-mobile-none">
-                        <a class="dashboard-link grid-x flex-center">
-                            <div class="cell small-2 flex-center">
-                                <feather-user></feather-user>
-                            </div>
-                            <p class="dashboard-regular cell small-10">Kontoübersicht</p>
-                        </a>
-                    </div>
-                    <div class="dashboard-menu">
-                        <a class="dashboard-link grid-x flex-center">
-                            <div class="cell small-2 flex-center">
-                                <feather-package></feather-package>
-                            </div>
-                            <p class="dashboard-regular cell small-10">Meine Bestellungen</p>
-                        </a>
+                @php($firstchar =  substr($customer->firstname, 0, 1))
+                @php($scndchar =  substr($customer->lastname, 0, 1))
 
-                        <a class="dashboard-link grid-x flex-center">
-                            <div class="cell small-2 flex-center">
-                                <feather-settings></feather-settings>
-                            </div>
-                            <p class="dashboard-regular cell small-10">Meine Angaben</p>
-                        </a>
-                    </div>
-                    <div class="dashboard-menu logout">
-                        <a class="dashboard-link grid-x flex-center" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            <div class="cell small-2 flex-center">
-                                <feather-logout></feather-logout>
-                            </div>
-                            <p class="dashboard-regular cell small-10">Abmelden</p>
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </div>
+                <dashboard-menu
+                    :customer-data="{{$customer}}"
+                    firstchar="{{$firstchar}}"
+                    scndchar="{{$scndchar}}"
+                    :overview-template="true"
+                ></dashboard-menu>
 
                 <div class="cell small-12 medium-6 large-8 display-mobile-none">
                     <img src="/img/dashboard-welcome.png" alt="Willkommen im Dashboard" class="dashboard-image">
