e9e11c62409bdd0f53b35694f60328a96601dd1c
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-19 12:30:35 +0200

    added icons for dashboard from feather

---
+++ resources/assets/js/components/FeatherPackage.vue
@@ -0,0 +1,30 @@
+<template>
+    <package-icon></package-icon>
+</template>
+
+<script>
+    import { Package } from 'vue-feather-icon'
+
+    export default {
+        data() {
+            return {}
+        },
+        mounted() {},
+        components: {
+            PackageIcon: Package
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/app.scss';
+
+    svg{
+        width: 20px;
+        stroke: $dark-grey;
+
+        &:hover{
+            stroke: $dark-grey;
+        }
+    }
+</style>
