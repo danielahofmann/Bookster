793b226ffd868a18f36bb2a8752ee5629334a52c
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-19 16:55:21 +0200

    added controller with store method

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    	$customer = Customer::find(Auth::user()->id);
+    	$customer->firstname = $request->input('firstname');
+    	$customer->lastname = $request->input('lastname');
+    	$customer->street = $request->input('street');
+    	$customer->housenum = $request->input('housenum');
+    	$customer->city = $request->input('city');
+    	$customer->postcode = $request->input('postcode');
+    	$customer->email = $request->input('email');
+    	$customer->save();
+
+	    return redirect()
+		    ->back()
+		    ->with('status', 'Benutzerdaten erfolgreich geändert');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
