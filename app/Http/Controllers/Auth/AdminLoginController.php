1851161f365cdb255bc1ca4f2ca66e794f304bd8
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-20 19:20:34 +0200

    when going to dashboard, now getting user data from auth

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -15,6 +15,8 @@ class AdminLoginController extends Controller
 		$this->middleware('guest:admin', ['except' => ['logout']]);
 	}
 
+	protected $guard = 'admin';
+
 	public function showLoginForm()
 	{
 		return view('admin.pages.login');
@@ -28,9 +30,11 @@ class AdminLoginController extends Controller
 			'password' => 'required|min:6'
 		]);
 
+		$remember = true;
 		// Attempt to log the user in
-		if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+		if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $remember)) {
 			// if successful, then redirect to their intended location
+
 			return redirect()->intended(route('admin.dashboard'));
 		}
 		// if unsuccessful, then redirect back to the login with the form data
@@ -40,6 +44,6 @@ class AdminLoginController extends Controller
 	public function logout()
 	{
 		Auth::guard('admin')->logout();
-		return redirect('/admin');
+		return redirect()->route('admin.login');
 	}
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,11 @@ Route::prefix('admin')->group(function() {
 	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 	Route::post('logout/', 'Auth\AdminLoginController@logout')->name('admin.logout');
 	Route::get('/', 'UserController@index')->name('admin.dashboard');
+
+	Route::get( '/', function () {
+		$user = Auth::guard('admin')->user();
+
+
+		return view('admin.pages.dashboard', ['user' => $user]);
+	})->name('admin.dashboard');
 });
