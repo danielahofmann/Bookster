0ba8cbb21fdd25cdc8b152246564b76dbf958513
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-20 21:07:29 +0200

    added feather edit icon

---
+++ resources/assets/js/components/FeatherEdit.vue
@@ -0,0 +1,29 @@
+<template>
+    <edit-icon></edit-icon>
+</template>
+
+<script>
+    import { Edit } from 'vue-feather-icon'
+
+    export default {
+        data() {
+            return {}
+        },
+        mounted() {
+            console.log('Component ready');
+        },
+        components: {
+            EditIcon: Edit
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/app.scss';
+
+    svg {
+        width: 20px;
+        height: 20px;
+        stroke: $white;
+    }
+</style>
