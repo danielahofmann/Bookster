432bf5d03a54fb29f32a1d2c416e7530f49da932
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-21 16:25:21 +0200

    added mobile nav to orders

--- resources/views/admin/pages/orders.blade.php
+++ resources/views/admin/pages/orders.blade.php
@@ -12,10 +12,6 @@
     <section class="beige fullscreen-beige-background admin-fullscreen no-padding-mobile">
         <div class="grid-container beige no-padding-mobile">
             <section class="fullscreen-beige-background grid-x grid-margin-x no-margin-mobile no-padding-mobile">
-                <div class="cell small-12 medium-6 large-8 display-mobile-only no-margin-mobile">
-                    <img src="/img/dashboard-welcome.png" alt="Willkommen im Admin-Dashboard" class="dashboard-image">
-                </div>
-
                 @php($firstchar =  substr($user->firstname, 0, 1))
                 @php($scndchar =  substr($user->lastname, 0, 1))
 
@@ -25,14 +21,25 @@
                         scndchar="{{$scndchar}}"
                         :order-view="true"
                         token="{!! csrf_token() !!}"
+                        class="display-mobile-none"
                 ></admin-dashboard-menu>
 
-                <div class="cell small-12 medium-6 large-8">
+                <div class="cell small-12 medium-6 large-8 no-margin-mobile">
                     <div class="dashboard-headline display-mobile-none">
                         <div>
                             <feather-package></feather-package>
                         </div>
                         <h2>Bestellungen</h2>
+                    </div>
+
+                    <div class="display-mobile-flex dashboard-redirect grid-x">
+                        <div class="cell small-2 flex-center">
+                            <a href="{{route('admin.dashboard')}}">
+                                <img src="/img/redirect-back.svg" alt="Zurück">
+                            </a>
+                        </div>
+
+                        <h2 class="cell small-10 text-left">Bestellungen</h2>
                     </div>
 
                     @foreach($orders as $order)
