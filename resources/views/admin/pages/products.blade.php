4332aebd87d0d8203feb1a6ce1e6eaeac7b38350
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-23 18:17:48 +0200

    defined basic template for products

---
+++ resources/views/admin/pages/products.blade.php
@@ -0,0 +1,46 @@
+@extends ('admin.admin')
+
+@section('title', 'Bestellungen' )
+
+@section('main')
+    @if(session('status'))
+        <alert-success-popup
+                status="{{session('status')}}"
+        ></alert-success-popup>
+    @endif
+
+    <section class="beige fullscreen-beige-background admin-fullscreen no-padding-mobile">
+        <div class="grid-container beige no-padding-mobile">
+            <section class="fullscreen-beige-background grid-x grid-margin-x no-margin-mobile no-padding-mobile">
+                @php($firstchar =  substr($user->firstname, 0, 1))
+                @php($scndchar =  substr($user->lastname, 0, 1))
+
+                <admin-dashboard-menu
+                        :user-data="{{$user}}"
+                        firstchar="{{$firstchar}}"
+                        scndchar="{{$scndchar}}"
+                        :product-view="true"
+                        token="{!! csrf_token() !!}"
+                        class="display-mobile-none"
+                ></admin-dashboard-menu>
+
+                <div class="cell small-12 medium-6 large-8 no-margin-mobile">
+
+                    <div class="dashboard-headline display-mobile-none">
+                        <div>
+                            <feather-book></feather-book>
+                        </div>
+                        <h2>Produkte</h2>
+                    </div>
+
+                    <admin-mobile-redirect
+                            headline="Produkte"
+                    ></admin-mobile-redirect>
+
+                    <div class="order-details">
+                        @foreach($products as $product)
+                            {{$product}}
+                            <hr>
+                            @endforeach
+                    </div>
+@stop
