f38f152c1fb7f20014697af1e35e0e01564dfdbe
DHofmann <daniela.hofmann3010@gmail.com>
2018-07-23 13:02:55 +0200

    changed to null

--- database/migrations/2018_05_10_175954_create_orders_table.php
+++ database/migrations/2018_05_10_175954_create_orders_table.php
@@ -18,7 +18,7 @@ class CreateOrdersTable extends Migration
             $table->integer('customer_id')->length(11);
             $table->integer('deliveryAddress_id')->length(11);
             $table->integer('billAddress_id')->length(11);
-            $table->integer('state_id')->length(11)->default('0');
+            $table->integer('state_id')->length(11)->default(0);
             $table->string('price', 28);
             $table->string('payment_method', 28);
             $table->string('shipping_method', 28);
