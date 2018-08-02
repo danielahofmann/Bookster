a58f01e37e76d71e4631e8e6d55a65db7f985f49
DHofmann <daniela.hofmann3010@gmail.com>
2018-08-02 20:08:21 +0200

    deleted old mail files bc they aren't used anymore

--- resources/views/mail/mails/order-saved.blade.php
+++
@@ -1,8 +0,0 @@
-@extends ('mail.mailmaster')
-
-@section('body')
-
-    <h1>Ihre Bestellung ist eingegangen</h1>
-    <p>Hiermit möchten wir Ihnen mitteilen, dass Ihre Bestellung erfolgreich eingegangen ist. Wir möchten Sie darauf hinweisen, dass die Bestellung erst nach Bestätigung durch die Erziehungsberechtigten rechtskräftig wird. Aus diesem Grund wird die Ware erst nach Bestätigungseingang bearbeitet.</p>
-
-@endsection
--- resources/views/mail/mails/order-successfully-confirmed.blade.php
+++
@@ -1,7 +0,0 @@
-@extends ('mail.mailmaster')
-
-@section('body')
-
-    <h1>Ihre Bestellung ist eingegangen</h1>
-    <p>Hiermit möchten wir Ihnen mitteilen, dass Ihre Bestellung erfolgreich bestätigt wurde und nun von uns bearbeitet wird.</p>
-@endsection
